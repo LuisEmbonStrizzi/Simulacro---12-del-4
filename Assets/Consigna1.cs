309199f1b460311eaa742e6aa1c7541de6517843
LuisEmbonStrizzi <luisembonstrizzi@gmail.com>
2022-04-12 08:53:11 -0300

    Consigna 1 y 2 terminadas

---
+++ Assets/Consigna1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consigna1 : MonoBehaviour
+{
+    public float montoAConvertir;
+    public char MonedaExtranjera;
+    float dolares;
+    float reales;
+    float euros;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dolares = montoAConvertir / 200;
+        reales = montoAConvertir / 23;
+        euros = montoAConvertir / 150;
+        if (montoAConvertir <= 1000)
+        {
+            Debug.Log("El monto mínimo es $1000");
+        }
+        else
+        {
+            switch (MonedaExtranjera)
+            {
+                case 'D':
+                    Debug.Log(montoAConvertir + " pesos argentinos equivalen a " + dolares + " dolares");
+                    break;
+
+                case 'R':
+                    Debug.Log(montoAConvertir + " pesos argentinos equivalen a " + reales + " reales");
+                    break;
+
+                case 'E':
+                    Debug.Log(montoAConvertir + " pesos argentinos equivalen a " + euros + " euros");
+                    break;
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Consigna2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consigna2 : MonoBehaviour
+{
+    public string nombreAlumno;
+    public int ano;
+    public char especialidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nombreAlumno.Length == 0 || ano < 1 && ano > 5)
+        {
+            Debug.Log("Error");
+        }
+        else if (especialidad != 'T' && especialidad != 'D' && especialidad != 'G' && especialidad != 'M' && especialidad != 'H')
+        {
+            Debug.Log("Solo puede ingresar T, D, G, M o H");
+        }
+        else if (ano < 3)
+        {
+            Debug.Log("Error. Aún estás en ciclo básico");
+        }
+        else
+        {
+            Debug.Log("Muchas gracias " + nombreAlumno);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
